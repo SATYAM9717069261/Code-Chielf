ad300095613684ab10cbad712581940c95570439
Satyam Mandal <satyam.mandal.77@gmail.com>
2021-06-25 03:57:42 +0530

    Even Number find in Febonicee Series

---
+++ fibonacci.cpp
@@ -0,0 +1,22 @@
+/**
+ * here we find even fibonacce number in give range
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 114 ...
+ * Even Numbers are => 0, 2, 8, 34, 114 ...
+ *
+ * relation is => 2*4+[previous even]
+ * 2*4+0=8
+ * 8*4+2=34
+ * 34*4+8=114
+ * ... soon
+ */ 
+#include<bits/stdc++.h>
+using namespace std;
+int febonic(int num,int& limit){
+	cout<<limit;
+}
+int main(int args, char** argv){
+	int limit=0;
+	cin>>limit;
+	febonic(0,limit)
+	return 0;
+}
